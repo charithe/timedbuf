dfb13ce5b0b219653879b2168b55e5824f2b7341
Lucian I. Last <li@last.nl>
2024-12-09 15:20:42 +0100

    Add v2 package and copy files from v1

---
+++ v2/timedbuf.go
@@ -0,0 +1,84 @@
+/*
+ *   Copyright 2016 Charith Ellawala
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package timedbuf
+
+import (
+	"sync"
+	"time"
+)
+
+//TimedBuf implements a buffer that gathers items until either the buffer size or a specified time limit is reached
+type TimedBuf struct {
+	mu          sync.Mutex
+	maxDelay    time.Duration
+	lastFlushTS time.Time
+	buffer      chan interface{}
+	ticker      *time.Ticker
+	flushFn     func([]interface{})
+}
+
+func New(size int, maxDelay time.Duration, flushFn func([]interface{})) *TimedBuf {
+	buffer := make(chan interface{}, size)
+	ticker := time.NewTicker(maxDelay)
+	tb := &TimedBuf{buffer: buffer, ticker: ticker, flushFn: flushFn, lastFlushTS: time.Now(), maxDelay: maxDelay}
+	tb.startLoop()
+	return tb
+}
+
+func (tb *TimedBuf) startLoop() {
+	go func() {
+		for _ = range tb.ticker.C {
+			tb.mu.Lock()
+			if time.Since(tb.lastFlushTS) > tb.maxDelay {
+				tb.doFlush()
+			}
+			tb.mu.Unlock()
+		}
+	}()
+}
+
+func (tb *TimedBuf) doFlush() {
+	bufLen := len(tb.buffer)
+	if bufLen > 0 {
+		tmp := make([]interface{}, bufLen)
+		for i := 0; i < bufLen; i++ {
+			tmp[i] = <-tb.buffer
+		}
+		tb.flushFn(tmp)
+		tb.lastFlushTS = time.Now()
+	}
+}
+
+func (tb *TimedBuf) Put(items ...interface{}) {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+	for _, i := range items {
+		select {
+		case tb.buffer <- i:
+		default:
+			tb.doFlush()
+			tb.buffer <- i
+		}
+	}
+}
+
+func (tb *TimedBuf) Close() {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+	tb.doFlush()
+	close(tb.buffer)
+	tb.ticker.Stop()
+}
---
+++ v2/timedbuf_test.go
@@ -0,0 +1,56 @@
+/*
+ *   Copyright 2016 Charith Ellawala
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package timedbuf
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlushOnFull(t *testing.T) {
+	var bufferContents []interface{}
+	tb := New(5, 30*time.Second, func(items []interface{}) { bufferContents = append(bufferContents, items...) })
+	for i := 0; i < 8; i++ {
+		tb.Put(fmt.Sprintf("item%d", i))
+		if i < 5 {
+			assert.Equal(t, 0, len(bufferContents))
+		}
+	}
+
+	// timer hasn't fired yet, so the buffer should only contain 5 items
+	assert.Equal(t, 5, len(bufferContents))
+	// after closing, the buffer should be completely emptied
+	tb.Close()
+	assert.Equal(t, 8, len(bufferContents))
+}
+
+func TestFlushOnTimer(t *testing.T) {
+	var bufferContents []interface{}
+	tb := New(5, 1*time.Second, func(items []interface{}) { bufferContents = append(bufferContents, items...) })
+	for i := 0; i < 3; i++ {
+		tb.Put(fmt.Sprintf("item%d", i))
+		assert.Equal(t, 0, len(bufferContents))
+	}
+
+	time.Sleep(1200 * time.Millisecond)
+	assert.Equal(t, 3, len(bufferContents))
+	tb.Close()
+	assert.Equal(t, 3, len(bufferContents))
+}
